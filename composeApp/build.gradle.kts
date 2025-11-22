49ebcb684fffd9bd318726477d846b9253cee9fc
Daniel Ehring <d.ehring@yahoo.com>
2025-11-22 20:59:26 +0100

    save commit

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -25,14 +25,14 @@ kotlin {
         }
     }
 
-    js(IR) {
-        browser {
-            commonWebpackConfig {
-                outputFileName = "parkmuc.js"
-            }
-        }
-        binaries.executable()
-    }
+//    js(IR) {
+//        browser {
+//            commonWebpackConfig {
+//                outputFileName = "parkmuc.js"
+//            }
+//        }
+//        binaries.executable()
+//    }
 
     sourceSets {
         androidMain.dependencies {
@@ -50,6 +50,17 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtimeCompose)
 
             implementation(libs.maplibre.compose)
+
+            val compassVersion = "1.0.0"
+            implementation("dev.jordond.compass:geocoder:${compassVersion}")
+            implementation("dev.jordond.compass:geocoder-mobile:${compassVersion}")
+// Geolocation
+            implementation("dev.jordond.compass:geolocation:${compassVersion}")
+// To use geolocation you need to use one or more of the following
+// Optional - Geolocation support for only iOS and Android
+            implementation("dev.jordond.compass:geolocation-mobile:${compassVersion}")
+// Optional - Location permissions for mobile (Android/iOS)
+            implementation("dev.jordond.compass:permissions-mobile:${compassVersion}")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- composeApp/src/commonMain/kotlin/de/madmuc/parkmuc/App.kt
+++ composeApp/src/commonMain/kotlin/de/madmuc/parkmuc/App.kt
@@ -6,19 +6,45 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import dev.jordond.compass.Location
+import dev.jordond.compass.Priority
+import dev.jordond.compass.geolocation.Geolocator
+import dev.jordond.compass.geolocation.GeolocatorResult
+import dev.jordond.compass.geolocation.Locator
+import dev.jordond.compass.geolocation.mobile
 import org.maplibre.compose.camera.CameraPosition
 import org.maplibre.compose.camera.rememberCameraState
+import org.maplibre.compose.expressions.dsl.const
+import org.maplibre.compose.layers.CircleLayer
 import org.maplibre.compose.map.MaplibreMap
+import org.maplibre.compose.sources.GeoJsonData
+import org.maplibre.compose.sources.rememberGeoJsonSource
 import org.maplibre.compose.style.BaseStyle
 import org.maplibre.spatialk.geojson.Position
 
 // https://maplibre.org/maplibre-compose/
 @Composable
 fun App() {
+    var userLocation by remember { mutableStateOf<Pair<Double, Double>?>( Pair(48.1373, 11.5753)) }
+    var test by remember { mutableStateOf<Double?>(null) }
+
+    val geolocator: Geolocator = Geolocator.mobile()
+
+    LaunchedEffect(Unit) {
+        val result: Location? = geolocator.current(Priority.HighAccuracy).getOrNull()
+        if (result != null) {
+            println("TEST"+result.accuracy)
+            userLocation = Pair(result.coordinates.latitude, result.coordinates.longitude)
+        } else {
+            println("Geolocation failed")
+        }
+    }
+
     MaterialTheme {
         // City Center of Munich (Marienplatz or something like that)
         val cameraState = rememberCameraState(
@@ -37,7 +63,31 @@ fun App() {
                 modifier = Modifier.fillMaxSize(),
                 baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty"),
                 cameraState = cameraState,
-            )
+            ) {
+                val markerSource = rememberGeoJsonSource(
+                    GeoJsonData.JsonString(
+                        """
+                        {
+                          "type": "Feature",
+                          "geometry": {
+                            "type": "Point",
+                            "coordinates": [${userLocation?.second}, ${userLocation?.first}]
+                          },
+                          "properties": {
+                            "name": "Marienplatz"
+                          }
+                        }
+                        """
+                    )
+                )
+
+                CircleLayer(
+                    id = "user",
+                    source = markerSource,
+                    color = const(Color(0xFFFFE222)),
+                    radius = const(10.dp)
+                )
+            }
 
             // Optional overlay UI
             Surface(
